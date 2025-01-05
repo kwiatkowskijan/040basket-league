5b4d2a256f83bd46ce256d155893c11f3e865b89
Jan Kwiatkowski <kwiatkojan@gmail.com>
2025-01-05 18:27:43 +0100

    Set up angular material

--- src/app/tournament-list/tournament-list.component.ts
+++ src/app/tournament-list/tournament-list.component.ts
@@ -4,11 +4,14 @@ import { Tournament } from '../models/tournament';
 import { TournamentService } from '../services/tournament.service';
 import { TournamentComponent } from '../tournament/tournament.component';
 import { RouterLink } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-tournament-list',
   standalone: true,
-  imports: [TournamentComponent, CommonModule, RouterLink],
+  imports: [TournamentComponent, CommonModule, RouterLink, FormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './tournament-list.component.html',
   styleUrls: ['./tournament-list.component.css']
 })
