2442350589793b0959fa85b7bb755ced7f4dbff0
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-01-14 18:42:13 +0100

    Added routing and team details component

--- src/app/tournament-team-details/tournament-team-details/tournament-team-details.component.ts
+++ src/app/tournament-team-details/tournament-team-details/tournament-team-details.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tournament-team-details',
+  standalone: true,
   imports: [],
   templateUrl: './tournament-team-details.component.html',
   styleUrl: './tournament-team-details.component.css'
 })
 export class TournamentTeamDetailsComponent {
+  teamId!: string;
+  route: ActivatedRoute = inject(ActivatedRoute);
+
+  constructor() {
+    this.teamId = this.route.snapshot.params["id"];
+  }
 
 }
