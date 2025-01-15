a09712e5158b84600a7b5c5aae5a9d3054cab43a
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-01-15 23:41:03 +0100

    player list styles

--- src/app/players/players-list/players-list.component.ts
+++ src/app/players/players-list/players-list.component.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayersService } from '../../services/players.service';
 import { Player } from '../../models/player';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-players-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './players-list.component.html',
   styleUrl: './players-list.component.css'
 })
 export class PlayersListComponent {
 
   playerList!: Player[];
+  tableColumHeaders: string[] = ['fullname', 'mail']
 
   constructor(private playersServie: PlayersService) {
     this.playersServie.getAllPlayers().then((playerList: Player[]) => {
