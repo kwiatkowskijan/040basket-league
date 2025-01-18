661dfa2775ff48f336a8f7e86406f3e2aead23b5
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-01-18 11:11:10 +0100

    Team details structure

--- src/app/tournament-team-details/tournament-team-details/tournament-team-details.component.ts
+++ src/app/tournament-team-details/tournament-team-details/tournament-team-details.component.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Team } from '../../models/team';
 import { TeamsService } from '../../services/teams.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-tournament-team-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './tournament-team-details.component.html',
   styleUrl: './tournament-team-details.component.css'
 })
@@ -18,7 +22,7 @@ export class TournamentTeamDetailsComponent {
   team: Team | undefined;
 
   constructor() {
-    this.teamId = this.route.snapshot.params["id"];
+    this.teamId = this.route.snapshot.params["id2"];
 
     this.teamService.getTeamById(this.teamId).then(team => {
       this.team = team;
