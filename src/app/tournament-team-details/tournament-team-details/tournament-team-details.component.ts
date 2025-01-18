24ce1c8cfc393c255e47cefd2b6c7611c20c776a
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-01-18 12:11:31 +0100

    Adding/editing teams

--- src/app/tournament-team-details/tournament-team-details/tournament-team-details.component.ts
+++ src/app/tournament-team-details/tournament-team-details/tournament-team-details.component.ts
@@ -3,29 +3,90 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Team } from '../../models/team';
 import { TeamsService } from '../../services/teams.service';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-tournament-team-details',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './tournament-team-details.component.html',
   styleUrl: './tournament-team-details.component.css'
 })
 export class TournamentTeamDetailsComponent {
   teamId!: string;
+  tournamentId!: string;
   route: ActivatedRoute = inject(ActivatedRoute);
   teamService = inject(TeamsService);
   team: Team | undefined;
+  isEditing = false;
+  isNew = false;
+
+  teamForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    city: new FormControl('', [Validators.required, Validators.maxLength(50)])
+  })
 
   constructor() {
+    this.tournamentId = this.route.snapshot.params["id"];
     this.teamId = this.route.snapshot.params["id2"];
 
-    this.teamService.getTeamById(this.teamId).then(team => {
-      this.team = team;
-    });
+    if(this.teamId === undefined) {
+      this.isNew = true;
+      this.isEditing = true;
+    }
+
+    if (this.isNew) {
+      this.team = {} as Team;
+    } else {
+      this.teamService.getTeamById(this.teamId).then(team => {
+        this.team = team;
+
+        this.teamForm.setValue({
+          name: this.team?.name ?? '',
+          city: this.team?.city ?? ''
+        })
+      });
+    }
+  }
+
+  turnOnEditMode() {
+    this.isEditing = true;
+  }
+
+  createEditTeam(form: FormGroup) {
+    if(this.team) {
+      this.team.name = form.value.name ?? '';
+      this.team.city = form.value.city ?? '';
+      this.team.tournamentId = this.tournamentId;
+
+      if(this.isNew) {
+        this.teamService.createTeam(this.team).subscribe({
+          next: (data) => {
+            this.team = data;
+            this.isNew = false;
+            this.isEditing = false;
+            console.log("Adding succesful!")
+          },
+          error: (error) => {
+            console.error('Error fetching posts:', error);
+          }
+        });
+      } else {
+        this.teamService.editTeam(this.team).subscribe({
+          next: (data) => {
+            this.team = data;
+            this.isEditing = false;
+            console.log("Update succesful!");
+          },
+          error: (error) => {
+            console.error('Error fetching posts:', error);
+          }
+        })
+      }
+    }
   }
 }
