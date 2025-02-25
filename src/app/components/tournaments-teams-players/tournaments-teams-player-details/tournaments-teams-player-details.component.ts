d8a2cfed51ea8ab3adcd606351c8410bf8c73dbf
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-02-25 17:01:50 +0100

    Tournaments teams player details component

--- src/app/components/tournaments-teams-players/tournaments-teams-player-details/tournaments-teams-player-details.component.ts
+++ src/app/components/tournaments-teams-players/tournaments-teams-player-details/tournaments-teams-player-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink, Router } from '@angular/router';
+import { TournamentsTeamsPlayer } from '../../../models/tournaments-teams-player';
+import { TournamentTeamsPlayersService } from '../../../services/tournament-teams-players.service';
 
 @Component({
   selector: 'app-tournaments-teams-player-details',
@@ -7,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './tournaments-teams-player-details.component.css'
 })
 export class TournamentsTeamsPlayerDetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  tournamentTeamsPlayersService = inject(TournamentTeamsPlayersService);
+  tournamentId!: number;
+  teamId!: number;
+  playerId!: number;
 
+  player: TournamentsTeamsPlayer | undefined;
+
+  constructor(private router: Router) {
+    this.tournamentId = this.route.snapshot.params["id"];
+    this.teamId = this.route.snapshot.params["id2"];
+    this.playerId = this.route.snapshot.params["id3"];
+
+    this.tournamentTeamsPlayersService.getPlayerByTeam(this.tournamentId, this.teamId, this.playerId).then(team => {
+      this.player = team;
+    });
+  }
 }
