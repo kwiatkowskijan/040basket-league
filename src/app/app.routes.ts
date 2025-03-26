51270f23e2cf6a145e9c0377b9b1fd7281f63853
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-03-26 21:47:31 +0100

    Basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -73,6 +73,6 @@ export const routes: Routes = [
     {
         path: 'tournaments/public/:id',
         component: TournamentPublicSiteComponent,
-        title: 'Player'
+        title: 'Tournament Public'
     }
 ];
--- src/app/components/public/tournament-public-site/tournament-public-site.component.ts
+++ src/app/components/public/tournament-public-site/tournament-public-site.component.ts
@@ -3,13 +3,29 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Tournament } from '../../../models/tournament';
 import { TournamentService } from '../../../services/tournament.service';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-tournament-public-site',
-  imports: [],
+  standalone: true,
+  imports: [MatIcon],
   templateUrl: './tournament-public-site.component.html',
   styleUrl: './tournament-public-site.component.css'
 })
 export class TournamentPublicSiteComponent {
+  tournament?: Tournament;
+  tournamentService = inject(TournamentService);
+  tournamentId!: number;
 
+  constructor(private route: ActivatedRoute) {}
+
+  async ngOnInit() {
+    this.tournamentId = this.route.snapshot.params["id"];
+
+    try {
+      this.tournament = await this.tournamentService.getTournamentById(this.tournamentId);
+    } catch (error) {
+      console.error(`Error while loading tournament: ${error}`)
+    }
+  }
 }
